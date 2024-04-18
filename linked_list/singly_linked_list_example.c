5f078002642bee7d55f90768653a19bdf762f322
Prathamesh Deshpande <deshpandeprathamesh2003@gmail.com>
2024-04-19 01:48:44 +0530

    README

--- linked_list/singly_linked_list_example.c
+++ linked_list/singly_linked_list_example.c
@@ -15,7 +15,6 @@ int main(int argc, char const *argv[])
     insert_after_intNode(in,1);
     print_SLL_intNode(in,printInt);
     putchar('\n');
-    insert_at_head_intNode(&in,-1);
     insert_at_end_intNode(in,2);
     print_SLL_intNode(in,printInt);
     putchar('\n');
